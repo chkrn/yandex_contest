ce276965a5ec53d807e1fa603e4fc780dde43be1
volkhin.s <sergey@volh.in>
2020-04-29 11:59:05 +0300

    Calc: Store priority in map and dont push brackets to list

--- z_bonus_calc.cpp
+++ z_bonus_calc.cpp
@@ -7,6 +7,9 @@
 #include <iostream>
 #include <sstream>
 #include <list>
+#include <map>
+#include <vector>
+#include <utility>
 
 enum class Reading {
 	start,
@@ -34,8 +37,16 @@ static void printList(std::list<int> list) {
 	std::cout << std::endl;
 }
 
-static std::list<int> readToList() {
-	std::list<int> rval;
+static std::pair<
+					std::list<int>,
+					std::map<
+								unsigned,
+								std::vector<std::list<int>::iterator>
+							>
+				> readToList() {
+
+	std::list<int> list;
+	std::map<unsigned, std::vector<std::list<int>::iterator>> prio;
 
 	char s;
 	int integerInput = 0;
@@ -54,19 +65,24 @@ static std::list<int> readToList() {
 				throw std::runtime_error("Operator not after integer value or right bracket");
 
 			if(reading == Reading::integer) {
-				rval.push_back(integerInput);
+				list.push_back(integerInput);
 				integerInput = 0;
 			}
 
-			rval.push_back(-s);
+			list.push_back(-s);
 			reading = Reading::oper;
+
+			unsigned curPrio = leftBrackets * 2;
+			if(s == '*' || s == '/')
+				curPrio += 1;
+			prio[curPrio].push_back(std::prev(list.end()));
+			
 		} else if(s == '(') {
 			leftBrackets++;
 
 			if(reading != Reading::start && reading != Reading::leftBracket && reading != Reading::oper)
 				throw std::runtime_error("Left bracket not after operator or left bracket or at start");
 
-			rval.push_back(-s);
 			reading = Reading::leftBracket;
 		} else if(s == ')') {
 			rightBrackets++;
@@ -78,11 +94,10 @@ static std::list<int> readToList() {
 				throw std::runtime_error("Detected more right brackets than left");
 
 			if(reading == Reading::integer) {
-				rval.push_back(integerInput);
+				list.push_back(integerInput);
 				integerInput = 0;
 			}
 
-			rval.push_back(-s);
 			reading = Reading::rightBracket;
 		}
 	}
@@ -94,11 +109,12 @@ static std::list<int> readToList() {
 		throw std::runtime_error("Left and right brackets nums mismatch (after all input analyze)");
 
 	if(reading == Reading::integer) {
-		rval.push_back(integerInput);
+		list.push_back(integerInput);
 		integerInput = 0;
 	}
 
-	return rval;
+	return std::pair<std::list<int>, std::map<unsigned, std::vector<std::list<int>::iterator>>>
+		(std::move(list), std::move(prio));
 }
 
 int count(const std::list<int>& list, std::list<int>::const_iterator& i) {
@@ -134,14 +150,14 @@ int count(const std::list<int>& list, std::list<int>::const_iterator& i) {
 
 int main() {
 try {
-	auto list = readToList();
+	auto listAndPrio = readToList();
 
 	std::cout << "Input:" << std::endl;
-	printList(list);
+	printList(listAndPrio.first);
 
-	std::cout << "Answer:" << std::endl;
-	std::list<int>::const_iterator begin = list.begin();
-	std::cout << count(list, begin) << std::endl;
+	//std::cout << "Answer:" << std::endl;
+	//std::list<int>::const_iterator begin = list.begin();
+	//std::cout << count(list, begin) << std::endl;
 
 	return 0;
 } catch(const std::runtime_error& e) {
